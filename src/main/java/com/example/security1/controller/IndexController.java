0e09901588bd34fbbc46092dad8e78d29c68ef74
siezn <seq0614@naver.com>
2022-10-17 15:46:03 +0900

    환경설정 완료

---
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -0,0 +1,50 @@
+package com.example.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    @ResponseBody
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    @ResponseBody
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc(){
+        return "회원가입 완료!";
+    }
+}
